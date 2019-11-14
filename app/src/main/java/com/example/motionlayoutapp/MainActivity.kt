10f2eb416e01e042834404e9818f0ec417b4a011
nahomtekeste <46280184+nahomtekeste@users.noreply.github.com>
2019-11-14 18:23:21 +0000

    Initial commit

---
+++ app/src/main/java/com/example/motionlayoutapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.motionlayoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        coordinateMotion()
+    }
+
+    // this functiuon allow us to move the limage
+    private fun coordinateMotion() {
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+
+        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+        appBarLayout.addOnOffsetChangedListener(listener)
+    }
+}
